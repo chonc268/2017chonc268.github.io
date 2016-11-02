f797dc21de35d7a9c220578ed328b5fad05259ea
Carol Chong <cchong1232@Carols-MacBook-Pro.local>
2016-11-02 02:05:07 -0400

    initial push

--- iteration_1/script.js
+++ iteration_1/script.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
-
-	//apend new element 100 times
-	for (i = 0; i < 10; i++) { 
-  $("body").append( "<div class='element' style='background-color: hsl(" + i * 3.6 + ", 100%, 50%);'></div>" );
-
-console.log("element " + i + " added");
-}
-	//mouse interaction
-	$('.element').mouseover (function() {
-		$(this).css('background-color','#9FD2CB');
-	});
-	$('.element').mouseout (function() {
-		$(this).css('background-color','#F6D6D3');
-	});
+$(document).ready(function() { //always do this first. A special event 
+  //append new element 100 times
+  for (i = 0; i < 1000; i++) { 
+    $("body").append( "<div class='element' </div>" );
+    console.log("element " + i + " added");
+  }
+  
+  //mouse interaction
+  $('.element').mouseover(function(){
+    $(this).css('background-color','gray');
+  });
+  $('.element').mouseout(function(){
+    $(this).css('background-color','gray');
+  });
 });
